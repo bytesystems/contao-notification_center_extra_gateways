8d36aaf9514ef1699a7e6e8be346f9dd84da95e0
danielheim <heim@bytesystems.de>
2019-08-25 20:42:04 +0200

    path

--- dca/tl_nc_gateway.php
+++ dca/tl_nc_gateway.php
@@ -1,17 +1,21 @@
 <?php
 
-$arrDca = $GLOBALS['TL_DCA']['tl_nc_gateway'];
+$table = NotificationCenter\Model\Gateway::getTable();
 
+$GLOBALS['TL_DCA'][$table]['palettes']['zapier'] = '{title_legend},title,type;{gateway_legend},zapier_url';
 /**
  * Fields
  */
-$arrDca['palettes']['zapier'] = '{title_legend},title,type;{gateway_legend},zapier_url';
-$arrDca['fields']['zapier_url'] = array
-(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_nc_gateway']['zapier_url'],
-    'exclude'                 => true,
-    'search'                  => true,
-    'inputType'               => 'text',
-    'eval'                    => array('mandatory'=>true, 'rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'tl_class'=>'w50 wizard'),
+$GLOBALS['TL_DCA'][$table]['fields']['zapier_url'] = [
+    'label'     => &$GLOBALS['TL_LANG'][$table]['zapier_url'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => [
+        'mandatory'      => true,
+        'rgxp'           =>'url',
+        'decodeEntities' => true,
+        'maxlength'      => 255,
+        'tl_class'       => 'w50'
+    ],
     'sql'                     => "varchar(255) NOT NULL default ''"
-);
+];
