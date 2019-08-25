6c65390305aa4cf3f0bf86b3bd4dd2abfe2af182
danielheim <heim@bytesystems.de>
2019-08-25 20:47:53 +0200

    path

--- languages/de/tl_nc_gateway.php
+++ languages/de/tl_nc_gateway.php
@@ -5,8 +5,8 @@ $table = NotificationCenter\Model\Gateway::getTable();
 /**
  * Fields
  */
-$GLOBALS['TL_LANG'][$table]['zapier_url'][0] = 'Clockwork API Key';
-$GLOBALS['TL_LANG'][$table]['zapier_url'][1] = 'Geben Sie den API Key ein. Ggf. müssen Sie sich auf <a href="https://www.clockworksms.com" target="_blank">clockworksms.com</a> registrieren.';
+$GLOBALS['TL_LANG'][$table]['zapier_url'][0] = 'Zapier Webhook';
+$GLOBALS['TL_LANG'][$table]['zapier_url'][1] = 'Geben Sie die URL für Ihren Zapier-Webhook ein. Ggf. müssen Sie sich auf <a href="https://www.zapier.com" target="_blank">zapier.com</a> registrieren.';
 
 
 /**
