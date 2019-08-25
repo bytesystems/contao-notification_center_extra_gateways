2d0af19211a234fc7a70950c85577e512bf679d3
danielheim <heim@bytesystems.de>
2019-08-25 12:40:34 +0200

    Initial Commit

--- dca/tl_nc_gateway.php
+++ dca/tl_nc_gateway.php
@@ -6,11 +6,9 @@ $arrDca = $GLOBALS['TL_DCA']['tl_nc_gateway'];
  * Fields
  */
 $arrDca['palettes']['zapier'] = '{title_legend},title,type;{gateway_legend},zapier_url';
-$arrDca['fields']
-
-['zapier_url'] = array
+$arrDca['fields']['zapier_url'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_nc_gateway']['postmark_key'],
+    'label'                   => &$GLOBALS['TL_LANG']['tl_nc_gateway']['zapier_url'],
     'exclude'                 => true,
     'search'                  => true,
     'inputType'               => 'text',
